679404b418ced40152b6d3bd178c173aa455eb27
Victtor777 <victtor.eng.software@gmail.com>
2024-05-14 19:21:23 -0300

    Add Link, e página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { stripe } from "@/lib/stripe";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -35,14 +36,15 @@ export default function Home({ products }: HomeProps) {
       <HomeContainer ref={sliderRef} className="keen-slider">
         {products.map((product) => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
-              <Image src={product.imageUrl} alt="" width={520} height={480} />
-
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} alt="" width={520} height={480} />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           );
         })}
       </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <div>
-      <h1>Product: {JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+          repudiandae rerum quas, delectus praesentium minima, fugiat labore
+          libero nam ipsa provident porro. Voluptatem facere perspiciatis, vel
+          omnis sapiente vero delectus?
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
