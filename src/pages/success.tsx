4c99b9bb57ab6c48190957a18f8e1b7d5a5956ff
Victtor777 <victtor.eng.software@gmail.com>
2024-05-15 17:10:33 -0300

    Add estilos na página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,18 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua compra de
+        <strong> 3 camisetas</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href={"/"}>Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }
